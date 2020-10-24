4bd4f80c36976aa157bd3c2ca230d79c2b389aab
Marin Pranjić <marin@mpranjic.com>
2020-10-24 23:08:36 +0200

    Added Arcade seeds, integrated in db.sh setup

--- db.sh
+++ db.sh
@@ -3,3 +3,4 @@
 docker-compose exec -u postgres postgres dropdb gaudi
 docker-compose exec -u postgres postgres createdb gaudi
 psql -U postgres -p 5401 -h localhost gaudi < examples/arcade/_db_setup.sql
+psql -U postgres -p 5401 -h localhost gaudi < examples/arcade/seeds.sql
