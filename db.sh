606113245b171e9dc68aad63bf5066bde90d9759
Marin Pranjić <marin@mpranjic.com>
2020-10-24 22:53:07 +0200

    Add docker-compose file and db.sh (db setup script)

---
+++ db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker-compose exec -u postgres postgres dropdb gaudi
+docker-compose exec -u postgres postgres createdb gaudi
+psql -U postgres -p 5401 -h localhost gaudi < examples/arcade/_db_setup.sql
