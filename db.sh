a3177180a70674efd107a5319c3e14bb93d0a9e8
Marin Pranjić <marin@mpranjic.com>
2022-09-12 23:23:56 +0200

    Add new folder structure.
    Old files are currently under composer, needs some refactoring / renaming.

--- db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose exec -u postgres postgres dropdb gaudi
-docker-compose exec -u postgres postgres createdb gaudi
-psql -U postgres -p 5401 -h localhost gaudi < examples/arcade/_db_setup.sql
-psql -U postgres -p 5401 -h localhost gaudi < examples/arcade/seeds.sql
