d31a4e47617c78f5755b887ed7f9d6e7b961cef1
Marin Pranjić <marin@mpranjic.com>
2022-09-14 09:10:44 +0200

    Add i1 (iteration 1) Specification type

--- src/types/specification.ts
+++ src/types/specification.ts
@@ -1,1 +1,30 @@
-export type Specification = {};
+export type Specification = {
+  models: ModelSpec;
+};
+
+type ModelSpec = {
+  name: string;
+  fields: FieldSpec[];
+  references: ReferenceSpec[];
+  relations: RelationSpec[];
+};
+
+type FieldSpec = {
+  name: string;
+  type: string;
+  unique?: boolean;
+  nullable?: boolean;
+};
+
+type ReferenceSpec = {
+  name: string;
+  toModel: string;
+  unique?: boolean;
+  nullable?: boolean;
+};
+
+type RelationSpec = {
+  name: string;
+  fromModel: string;
+  through: string;
+};
