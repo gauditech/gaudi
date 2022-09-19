d831b85fd5bfa11137adaa62bab8a246090421eb
Hrvoje Kasabasic <hrvoje.kasabasic@mashpipe.io>
2022-09-19 23:27:46 +0200

    use compiler output in composer and builder

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import fs from "fs";
 
 import "./common/setupAliases";
+// import definitionInput from "@examples/git/definition.json";
+// import specificationInput from "@examples/git/specification.json";
 
-import definitionInput from "@examples/git/definition.json";
-import specificationInput from "@examples/git/specification.json";
-
-import { build } from "./builder/builder";
-import { compile } from "./compiler/compiler";
-import { compose } from "./composer/composer";
-import { parse } from "./parser/parser";
+import { build } from "@src/builder/builder";
+import { compile } from "@src/compiler/compiler";
+import { compose } from "@src/composer/composer";
+import { parse } from "@src/parser/parser";
 
 const blueprintPath = "./examples/git/blueprint.gaudi";
 
 const input = fs.readFileSync(blueprintPath).toString("utf-8");
 const ast = parse(input);
 const _specification = compile(ast);
-const _definition = compose(specificationInput);
-build(definitionInput as any);
+const _definition = compose(_specification);
+build(_definition);
