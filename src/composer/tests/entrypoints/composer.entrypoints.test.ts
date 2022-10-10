91f03eabfe14b2651cd9f4dace8bf7e4ffdac61d
Marin Pranjić <marin@mpranjic.com>
2022-10-10 12:25:33 +0200

    Move composer tests to jest snapshots

--- src/composer/tests/entrypoints/composer.entrypoints.test.ts
+++ src/composer/tests/entrypoints/composer.entrypoints.test.ts
@@ -1,5 +1,4 @@
 import { compile, compose, parse } from "@src/index";
-import { EntrypointDef } from "@src/types/definition";
 
 describe("entrypoint", () => {
   it("composes basic example", () => {
@@ -31,98 +30,7 @@ describe("entrypoint", () => {
       }
     }
     `;
-    const spec = compile(parse(bp));
-    const def = compose(spec);
-    const ep: EntrypointDef[] = [
-      {
-        target: {
-          kind: "model",
-          name: "Org",
-          refKey: "Org",
-          type: "Org",
-          identifyWith: { name: "slug", refKey: "Org.slug", type: "text" },
-          alias: null,
-        },
-        name: "Orgs",
-        endpoints: [
-          {
-            kind: "list",
-            response: {
-              fieldRefs: ["Org.id", "Org.name", "Org.slug"],
-              references: [],
-              relations: [],
-              queries: [],
-            },
-            actions: [],
-          },
-          {
-            kind: "get",
-            response: {
-              fieldRefs: ["Org.id", "Org.name", "Org.slug"],
-              references: [],
-              relations: [],
-              queries: [],
-            },
-            actions: [],
-          },
-        ],
-        entrypoints: [
-          {
-            name: "Orgs.Repositories",
-            target: {
-              kind: "relation",
-              name: "repos",
-              refKey: "Org.repos",
-              type: "Repo",
-              alias: "repo",
-              identifyWith: { name: "id", refKey: "Repo.id", type: "integer" },
-            },
-            endpoints: [
-              {
-                kind: "list",
-                response: {
-                  fieldRefs: ["Repo.id", "Repo.title", "Repo.org_id"],
-                  references: [],
-                  relations: [],
-                  queries: [],
-                },
-                actions: [],
-              },
-              {
-                kind: "get",
-                response: {
-                  fieldRefs: ["Repo.id", "Repo.title", "Repo.org_id"],
-                  references: [],
-                  relations: [],
-                  queries: [],
-                },
-                actions: [],
-              },
-              {
-                kind: "create",
-                fieldset: { fields: { title: { kind: "field", nullable: false, type: "text" } } },
-                contextActionChangeset: {
-                  org_id: {
-                    kind: "reference-value",
-                    type: "integer",
-                    target: { alias: "org", access: ["id"] },
-                  },
-                  title: { kind: "fieldset-input", type: "text", fieldsetAccess: ["title"] },
-                },
-                actions: [],
-                response: {
-                  fieldRefs: ["Repo.id", "Repo.title", "Repo.org_id"],
-                  references: [],
-                  relations: [],
-                  queries: [],
-                },
-              },
-            ],
-            entrypoints: [],
-          },
-        ],
-      },
-    ];
-    expect(def.entrypoints).toStrictEqual(ep);
+    const def = compose(compile(parse(bp)));
+    expect(def.entrypoints).toMatchSnapshot();
   });
 });
