c8c345957aa11cf4585ca25ed2cdb47d1dc939fa
Marin Pranjić <marin@mpranjic.com>
2022-11-17 09:42:03 +0100

    Move to a file snapshot

--- src/composer/tests/composer.customActions.test.ts
+++ src/composer/tests/composer.customActions.test.ts
@@ -26,40 +26,7 @@ describe("custom actions", () => {
     `;
     const def = compose(compile(parse(bp)));
     const endpoint = def.entrypoints[0].endpoints[0];
-    expect(endpoint.actions).toMatchInlineSnapshot(`
-      [
-        {
-          "alias": "e",
-          "changeset": {},
-          "kind": "create-one",
-          "model": "OrgExtra",
-          "response": [],
-        },
-        {
-          "alias": "org",
-          "changeset": {
-            "extras": {
-              "kind": "reference-value",
-              "target": {
-                "access": [
-                  "id",
-                ],
-                "alias": "e",
-              },
-              "type": "integer",
-            },
-            "is_new": {
-              "kind": "value",
-              "type": "boolean",
-              "value": true,
-            },
-          },
-          "kind": "create-one",
-          "model": "Org",
-          "response": [],
-        },
-      ]
-    `);
+    expect(endpoint.actions).toMatchSnapshot();
   });
   it.todo("fails when default action override is invalid type");
   it.todo("sets default action if not given");
