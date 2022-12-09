442329cabacb9c51794064ae1e40673954ebf1f1
Ivo Kosir <ivo.kosir93@gmail.com>
2022-12-09 13:05:17 +0100

    Use external package for testing ascii

--- examples/gaudi-githubc/hooks/githubc.js
+++ examples/gaudi-githubc/hooks/githubc.js
@@ -1,6 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const isAscii = require("is-ascii");
+
 module.exports.noUnicode = function ({ name }) {
-  for (var i = 0; i < name.length; i++) {
-    if (name.charCodeAt(i) > 127) return false;
-  }
-  return true;
+  return isAscii(name);
 };
