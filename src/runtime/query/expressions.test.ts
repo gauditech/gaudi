7d43fa0618485855a2b44098bbe6373a9bdee2de
Marin Pranjić <marin@mpranjic.com>
2022-12-24 23:06:42 +0100

    Add a test case for the composing of a computed prop

--- src/runtime/query/expressions.test.ts
+++ src/runtime/query/expressions.test.ts
@@ -3,7 +3,7 @@ import { queryToString } from "./stringify";
 import { compile, compose, parse } from "@src/index";
 
 describe("Expressions to queries", () => {
-  it("composes a complex expression", () => {
+  it("composes a complex filter expression", () => {
     const bp = `
     model Source {
       relation items { from Item, through source }
@@ -24,8 +24,29 @@ describe("Expressions to queries", () => {
 
     const def = compose(compile(parse(bp)));
     const source = def.models.find((m) => m.name === "Source");
-    const calc = source?.queries[0];
+    const calc = source!.queries[0]!;
     expect(calc).toMatchSnapshot();
-    expect(queryToString(def, calc!)).toMatchSnapshot();
+    expect(queryToString(def, calc)).toMatchSnapshot();
+  });
+  it("composes a complex local computed expression", () => {
+    const bp = `
+    model Source {
+      relation items { from Item, through source }
+    }
+    model Item {
+      reference source { to Source }
+      field value { type integer }
+      field multiplier { type integer }
+      field textual { type text }
+      computed worthiness {
+        multiplier * (value + 1) / length(concat(textual, "tail"))
+      }
+    }
+    `;
+
+    const def = compose(compile(parse(bp)));
+    const item = def.models.find((m) => m.name === "Item");
+    const worthiness = item!.computeds[0]!;
+    expect(worthiness).toMatchSnapshot();
   });
 });
