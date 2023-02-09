9506be412a661f80ab4adcdf6a10fa69e3c3b739
Marin Pranjić <marin@mpranjic.com>
2023-02-09 12:41:55 +0100

    Web style changes

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
 import React from "react";
 
 import styles from "./index.module.css";
+
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
