f9545d9cd6eef8a533ca4da12edee7b9aa27df14
Hrvoje Kasabasic <hrvoje.kasabasic@mashpipe.io>
2023-06-12 16:48:35 +0200

    remove "slugify" from template project

--- template-create-gaudi/template-vite-react-ts/app/server/src/hooks/hooks.ts
+++ template-create-gaudi/template-vite-react-ts/app/server/src/hooks/hooks.ts
@@ -1,5 +1,1 @@
-import slugifyImpl from "@sindresorhus/slugify";
-
-export function slugify({ value }: { value: string }) {
-  return value ? slugifyImpl(value) : "";
-}
+// Write your custom hooks code here ...
