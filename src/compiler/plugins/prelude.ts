55d8389287518c36327ed88a272c92c85cf0658c
Ivo Kosir <ivo.kosir93@gmail.com>
2023-07-03 11:52:28 +0200

    Update some codes

--- src/compiler/plugins/prelude.ts
+++ src/compiler/plugins/prelude.ts
@@ -4,64 +4,64 @@ function getCode(): string {
     arg value { type integer }
     arg max { type integer }
     assert { value <= max }
-    error { code "max-int" }
+    error { code "too-large" }
   }
   validator minInt {
     arg value { type integer }
     arg min { type integer }
     assert { value >= min }
-    error { code "min-int" }
+    error { code "too-small" }
   }
 
   validator maxFloat {
     arg value { type float }
     arg max { type float }
     assert { value <= max }
-    error { code "max-float" }
+    error { code "too-large" }
   }
   validator minFloat {
     arg value { type float }
     arg min { type float }
     assert { value >= min }
-    error { code "min-float" }
+    error { code "too-small" }
   }
 
   validator maxLength {
     arg value { type string }
     arg min { type integer }
     assert { length(value) <= min }
-    error { code "max-length" }
+    error { code "too-long" }
   }
   validator minLength {
     arg value { type string }
     arg max { type integer }
     assert { length(value) >= max }
-    error { code "min-length" }
+    error { code "too-short" }
   }
 
   validator isEqualBool {
     arg value { type boolean }
     arg target { type boolean }
     assert { value is target }
-    error { code "is-equal-bool" }
+    error { code "not-equal" }
   }
   validator isEqualInt {
     arg value { type integer }
     arg target { type integer }
     assert { value is target }
-    error { code "is-equal-int" }
+    error { code "not-equal" }
   }
   validator isEqualFloat {
     arg value { type float }
     arg target { type float }
     assert { value is target }
-    error { code "is-equal-float" }
+    error { code "not-equal" }
   }
   validator isEqualString {
     arg value { type string }
     arg target { type string }
     assert { value is target }
-    error { code "is-equal-string" }
+    error { code "not-equal" }
   }
 
   validator isEmail {
@@ -71,7 +71,7 @@ function getCode(): string {
       // https://www.regular-expressions.info/email.html
       inline "value.match(/[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\\\\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)"
     }
-    error { code "is-email" }
+    error { code "invalid-email" }
   }
   `;
 }
